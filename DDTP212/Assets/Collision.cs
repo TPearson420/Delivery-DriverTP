fa3c2c1315c800ba43aef94f05cf20705148acf0
TPearson420 <pearso420@morrisville.edu>
2023-09-10 02:58:00 -0400

    OnCollisionEnter2D

---
+++ DDTP212/Assets/Collision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+
+    void  nCollisionEnter2D(Collision2D collision)              {
+        Debug.Log("Ouch!");
+   
+    
+    
+    
+    
+    
+    }
+}
